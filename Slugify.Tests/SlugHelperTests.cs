9765769ebf76a597fc9c92cbca4e9161550ca414
Federico Cingolani <mail@fcingolani.com.ar>
2013-07-29 17:19:20 -0300

    First commit

---
+++ Slugify.Tests/SlugHelperTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Slugify.Tests
+{
+
+    [TestClass]
+    public class SlugHelperTest
+    {
+
+        [TestMethod]
+        public void TestEmptyConfig()
+        {
+            SlugHelper.Config config = new SlugHelper.Config();
+            Assert.IsTrue(config.ForceLowerCase);
+            Assert.IsTrue(config.CollapseWhiteSpace);
+            Assert.AreEqual(0, config.CharacterReplacements.Count);
+            Assert.IsNotNull(new Regex(config.DeniedCharactersRegex));
+        }
+
+        [TestMethod]
+        public void TestDefaultConfig()
+        {
+            KeyValuePair<string, string> defaultReplacement = new KeyValuePair<string, string>(" ","-");
+            
+            SlugHelper.Config config = new SlugHelper.Config.Default();
+            
+            Assert.AreEqual(1, config.CharacterReplacements.Count);
+            Assert.AreEqual("-", config.CharacterReplacements[" "]);
+
+        }
+
+        [TestMethod]
+        public void TestEmptyConstructor()
+        {
+            SlugHelper helper = new SlugHelper();
+            Assert.IsNotNull(helper);
+        }
+
+        [TestMethod]
+        public void TestLoweCaseEnforcement()
+        {
+            String original = "AbCdE";
+            String expected = "abcde";
+
+            SlugHelper helper = new SlugHelper();
+
+            Assert.AreEqual(expected, helper.GenerateSlug(original));
+        }
+
+        [TestMethod]
+        public void TestWhiteSpaceCollapsing()
+        {
+            String original = "a  b    \n  c   \t    d";
+            String expected = "a-b-c-d";
+
+            SlugHelper helper = new SlugHelper();
+
+            Assert.AreEqual(expected, helper.GenerateSlug(original));
+        }
+        
+        [TestMethod]
+        public void TestDiacriticRemoval()
+        {
+            String withDiacritics = "ñáîùëÓ";
+            String withoutDiacritics = "naiueo";
+
+            SlugHelper helper = new SlugHelper();
+
+            Assert.AreEqual(withoutDiacritics, helper.GenerateSlug(withDiacritics));
+        }
+
+        [TestMethod]
+        public void TestDeniedCharacterDeletion()
+        {
+            String original = "!#$%&/()=";
+            String expected = "";
+
+            SlugHelper helper = new SlugHelper();
+
+            Assert.AreEqual(expected, helper.GenerateSlug(original));
+        }
+
+        [TestMethod]
+        public void TestCharacterReplacement()
+        {
+            String original = "abcde";
+            String expected = "xyzde";
+
+            SlugHelper.Config config = new SlugHelper.Config();
+            config.CharacterReplacements.Add("a", "x");
+            config.CharacterReplacements.Add("b", "y");
+            config.CharacterReplacements.Add("c", "z");
+
+            SlugHelper helper = new SlugHelper(config);
+
+            Assert.AreEqual(expected, helper.GenerateSlug(original));
+        }
+
+        [TestMethod]
+        public void TestFullFunctionality()
+        {
+            SlugHelper helper = new SlugHelper();
+            Dictionary<string, string> tests = new Dictionary<string, string>();
+            
+            tests.Add(  "E¢Ðƕtoy  mÚÄ´¨ss¨sïuy   !!!!!  Pingüiño",
+                        "etoy-muasssiuy--pinguino");
+
+            tests.Add(  "QWE dfrewf# $%&!! asd",
+                        "qwe-dfrewf--asd");
+            
+            tests.Add(  "You can't have any pudding if you don't eat your meat!",
+                        "you-cant-have-any-pudding-if-you-dont-eat-your-meat");
+
+            tests.Add(  "El veloz murciélago hindú",
+                        "el-veloz-murcielago-hindu");
+
+            tests.Add(  "Médicos sin medicinas medican meditando",
+                        "medicos-sin-medicinas-medican-meditando");
+
+            foreach(KeyValuePair<string, string> test in tests){
+                Assert.AreEqual(test.Value, helper.GenerateSlug(test.Key));
+            }
+        }
+
+    }
+}
---
+++ Slugify/SlugHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Slugify
+{
+
+    public class SlugHelper
+    {
+        protected Config _config { get; set; }
+
+        public SlugHelper() :
+            this(new SlugHelper.Config.Default())
+        {
+
+        }
+
+        public SlugHelper(Config config)
+        {
+            _config = config;
+        }
+
+        public String GenerateSlug(String str)
+        {
+            if (_config.ForceLowerCase)
+                str = str.ToLower();
+
+            str = CleanWhiteSpace(str, _config.CollapseWhiteSpace);
+            str = ApplyReplacements(str, _config.CharacterReplacements);
+            str = RemoveDiacritics(str);
+            str = DeleteCharacters(str, _config.DeniedCharactersRegex);
+
+            return str;
+        }
+
+        protected String CleanWhiteSpace(String str, Boolean collapse)
+        {
+            return Regex.Replace(str, collapse ? @"\s+" : @"\s", " ");
+        }
+
+        // Thanks http://stackoverflow.com/a/249126!
+        protected String RemoveDiacritics(String str)
+        {
+            string stFormD = str.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            for (int ich = 0; ich < stFormD.Length; ich++)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(stFormD[ich]);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(stFormD[ich]);
+                }
+            }
+
+            return (sb.ToString().Normalize(NormalizationForm.FormC));
+        }
+
+        protected String ApplyReplacements(String str, Dictionary<string, string> replacements)
+        {
+            StringBuilder sb = new StringBuilder(str);
+
+            foreach (KeyValuePair<string, string> replacement in replacements)
+                sb.Replace(replacement.Key, replacement.Value);
+
+            return sb.ToString();
+        }
+
+        protected String DeleteCharacters(String str, String regex)
+        {
+            return Regex.Replace(str, regex, "");
+        }
+
+        public class Config
+        {
+            public Dictionary<String, String> CharacterReplacements { get; set; }
+            public Boolean ForceLowerCase { get; set; }
+            public Boolean CollapseWhiteSpace { get; set; }
+            public String DeniedCharactersRegex { get; set; }
+
+            public Config()
+            {
+                CharacterReplacements = new Dictionary<string, string>();
+                ForceLowerCase = true;
+                CollapseWhiteSpace = true;
+                DeniedCharactersRegex = @"[^a-zA-Z0-9\-\._]";
+            }
+
+            public class Default : Config
+            {
+                public Default()
+                {
+                    CharacterReplacements.Add(" ", "-");
+                }
+            }
+        }
+
+    }
+
+}
+
