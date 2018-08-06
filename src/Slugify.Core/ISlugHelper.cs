8a36e0e8532c13242fbd30a8d9d7a814e9fd3eb3
James Charlesworth <jcharlesworthuk@users.noreply.github.com>
2018-08-06 22:44:14 +0100

    Add ISlugHelper interface and move comment (#7)

---
+++ src/Slugify.Core/ISlugHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Slugify
+{
+    public interface ISlugHelper
+    {
+        /// <summary>
+        /// Generates a slug from the provided <paramref name="inputString"/>
+        /// </summary>
+        /// <param name="inputString">The string to slugify</param>
+        /// <returns>A slugified version of <paramref name="inputString"/></returns>
+        string GenerateSlug(string inputString);
+    }
+}
