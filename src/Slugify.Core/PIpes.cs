5ab8ec4f78e234fc5180894a130d62f0bc90f4b3
Benjamin Boyle <bboyle1234@gmail.com>
2020-04-30 08:36:53 -0400

    Initial WIP

---
+++ src/Slugify.Core/PIpes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slugify.Core {
+
+    abstract class CharPipe {
+
+        protected CharPipe Continuation;
+
+        public T Continue<T>(T continuation) where T : CharPipe {
+            Continuation = continuation;
+            return continuation;
+        }
+
+        public abstract void Write(char c);
+        public abstract void Complete();
+    }
+
+    class NoOpCharPipe : CharPipe {
+        public override void Write(char c) => Continuation.Write(c);
+        public override void Complete() => Continuation.Complete();
+    }
+
+    class ToLowerCharPipe : CharPipe {
+        public override void Write(char c) => Continuation.Write(char.ToLower(c));
+        public override void Complete() => Continuation.Complete();
+    }
+
+    class ReplaceCharPipe : CharPipe {
+
+        readonly char[] Search;
+        readonly char[] Replace;
+
+        int matchCount = 0;
+
+        public ReplaceCharPipe(string search, string replace) {
+            Search = search.ToCharArray();
+            Replace = replace.ToCharArray();
+        }
+        public override void Write(char c) {
+            if (c == Search[matchCount]) {
+                matchCount++;
+                if (matchCount == Search.Length) {
+                    for (var i = 0; i < Replace.Length; i++)
+                        Continuation.Write(Replace[i]);
+                    matchCount = 0;
+                }
+            } else {
+                for (var i = 0; i < matchCount; i++)
+                    Continuation.Write(Search[i]);
+                Continuation.Write(c);
+                matchCount = 0;
+            }
+        }
+        public override void Complete() {
+            for (var i = 0; i < matchCount; i++) {
+                Continuation.Write(Search[i]);
+            }
+            Continuation.Complete();
+        }
+    }
+
+    class TrimCharPipe : CharPipe {
+        readonly List<char> WhiteSpaceBuffer = new List<char>();
+        bool hasStartedChars = false;
+        public override void Write(char c) {
+            if (hasStartedChars) {
+                if (char.IsWhiteSpace(c)) {
+                    WhiteSpaceBuffer.Add(c);
+                } else {
+                    foreach (var c2 in WhiteSpaceBuffer)
+                        Continuation.Write(c2);
+                    WhiteSpaceBuffer.Clear();
+                    Continuation.Write(c);
+                }
+            } else {
+                if (!char.IsWhiteSpace(c)) {
+                    Continuation.Write(c);
+                    hasStartedChars = true;
+                }
+            }
+        }
+        public override void Complete() => Continuation.Complete();
+    }
+
+    class ResultCharPipe : CharPipe {
+        readonly StringBuilder sb = new StringBuilder();
+        public override void Write(char c) => sb.Append(c);
+        public override void Complete() => Continuation.Complete();
+        public string GetResult() => sb.ToString();
+    }
+}
---
+++ src/Slugify.Core/PIpesSlugHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slugify.Core {
+    class PipesSlugHelper : ISlugHelper {
+
+        readonly SlugHelper.Config Config;
+
+        readonly CharPipe StartPipe;
+        readonly ResultCharPipe ResultCharPipe;
+
+        public PipesSlugHelper(SlugHelper.Config config) {
+            Config = config;
+            var currentPipe = StartPipe = new NoOpCharPipe();
+
+            if (Config.TrimWhitespace)
+                currentPipe = currentPipe.Continue(new TrimCharPipe());
+
+            ResultCharPipe = currentPipe.Continue(new ResultCharPipe());
+        }
+
+        public string GenerateSlug(string inputString) {
+            foreach (var c in inputString)
+                StartPipe.Write(c);
+            StartPipe.Complete();
+            return ResultCharPipe.GetResult();
+        }
+    }
+}
